d5778c5e37d0d068c4a38d62209757dd3dacaef6
Craig Mayhew <craig@mayhew.io>
2024-01-23 20:37:51 +0000

    Refactored after bumping lambda_runtime create versions 0.4 -> 0.9Bumped simple logger version 1 > 4

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,10 @@ extern crate serde_derive;
 extern crate log;
 extern crate simple_logger;
 
-use lambda::{handler_fn, Context, Error};
+use lambda_runtime::{service_fn, LambdaEvent, Error};
 use log::LevelFilter;
-use serde::{Deserialize, Serialize};
+use serde_json::{json,Value};
 use simple_logger::SimpleLogger;
-
-#[derive(Deserialize, Clone)]
-struct Request {
-    #[serde(rename = "name")]
-    name: String,
-    #[serde(rename = "phone")]
-    phone: String,
-    #[serde(rename = "email")]
-    email: String,
-    #[serde(rename = "message")]
-    message: String,
-}
-
-#[derive(Serialize)]
-struct Response {
-    req_id: String,
-    msg: String,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -33,39 +15,37 @@ async fn main() -> Result<(), Error> {
     // can be replaced with any other method of initializing `log`
     SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
 
-    let func = handler_fn(my_handler);
+    let func = service_fn(my_handler);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-pub(crate) async fn my_handler(e: Request, ctx: Context) -> Result<Response, Error> {
+async fn my_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let (e, ctx) = event.into_parts();
     let mut output_msg = "";
-    let name = e.name.as_str();
+    let name = e["name"].as_str().unwrap_or("Empty name in request");
     if name == "" {
         error!("Empty name in request {}", ctx.request_id);
         output_msg = "Error: name is empty."
     }
 
-    let phone = e.phone.as_str();
+    let phone = e["phone"].as_str().unwrap_or("Empty phone number in request");
     if phone == "" {
         error!("Empty phone number in request {}", ctx.request_id);
         output_msg = "Error: phone is empty."
     }
 
-    let email = e.email.as_str();
+    let email = e["email"].as_str().unwrap_or("Empty E-Mail in request");
     if email == "" {
         error!("Empty E-Mail in request {}", ctx.request_id);
         output_msg = "Error: E-Mail is empty."
     }
 
-    let message = e.message.as_str();
+    let message = e["message"].as_str().unwrap_or("Empty message in request");
     if message == "" {
         error!("Empty message in request {}", ctx.request_id);
         output_msg = "Error: message is empty."
     }
 
-    Ok(Response {
-        req_id: ctx.request_id,
-        msg: output_msg.to_string(),
-    })
+    Ok(json!({ "message": output_msg.to_string() }))
 }
